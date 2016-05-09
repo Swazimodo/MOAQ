413d00725b9e6d8105f5073b19d908baf4c113da
Swazimodo <Swazimodo@users.noreply.github.com>
2016-05-09 12:15:15 -0600

    Update MOAQ.sql
    Add QUOTENAME to table and column names. Added dbo. to table reference.

--- MOAQ.sql
+++ MOAQ.sql
@@ -1,38 +1,40 @@
---Mother of all Queries 
+--Mother of all search queries 
 --Sam Nesbitt
+--v1
 --2016-03-01
 
 --user definable
-declare @colType      int
-declare @searchData   varchar(10)	--change this datatype as needed
-declare @searchOpp    varchar(10)
-declare @searchTable  varchar(100)
+declare @colType		int
+declare @searchData		varchar(50)
+declare @searchOpp		varchar(10)	--enter seach opperator
+declare @searchTable	varchar(100)
 
-set @colType = 61              --enter sql data type
-set @searchData = '2016-03-31' --Enter search data here			
-set @searchOpp = '>='          --Enter your search opporator here                          --'<='
-set @searchTable = '%'         --Give the query a hint to the column name its looking for  --'%start_dt%','%'
+set @colType = 61			--56:int, 61:datetime, 104:bit --http://avian.netne.net/blog/?p=277
+set @searchData = '''2000-03-31'''		--Enter search data here, add '' for text
+set @searchOpp = '<='				--Enter your search opporator here							--'<='
+set @searchTable = '%'				--Give the query a hint to the column name its looking for	--'%start_dt%','%'
 
+--table to store search results
 IF OBJECT_ID('tempdb..##searchResults') IS NOT NULL
 	DROP TABLE ##searchResults
 CREATE TABLE ##searchResults(
 	tableName	varchar(100)
 	,columnName	varchar(100)
-	,result		datetime		--make sure this data type matches @searchData data type
+	,result		datetime		--make sure this data type matches the type specified by @colType
 )
 
 --system use
-
 declare @tableName	varchar(100)
 declare @colName	varchar(100)
 
+--tables and columns that will be searched
 declare @searchTables table(
 	tableName	varchar(100)
 	,columnName	varchar(100)
 )
 
 insert into @searchTables
-select d.TableName, d.ColName
+select QUOTENAME(d.TableName), QUOTENAME(d.ColName)
 from
 	(SELECT c.name AS ColName, t.name AS TableName
 	FROM sys.columns c
@@ -59,16 +61,16 @@ begin
 	EXEC
 	( 
 		'INSERT INTO ##searchResults select ''' + @tableName + ''', ''' + @colName + ''', d.' + @colName + ' from' +
-		'(SELECT ' + @colName 
-		+ ' from ' + @tableName
-		+ ' where ' + @colName + ' ' + @searchOpp + ' ''' + @searchData + ''' ) as d'
+		'(SELECT ' + @colName
+		+ ' from dbo.' + @tableName
+		+ ' where ' + @colName + ' ' + @searchOpp + ' ' + @searchData + ' ) as d'
 	)
 	
 	--dump queries to log
 	print 'INSERT INTO ##searchResults select ''' + @tableName + ''', ''' + @colName + ''', d.' + @colName + ' from' +
-		'(SELECT ' + @colName 
-		+ ' from ' + @tableName
-		+ ' where ' + @colName + ' ' + @searchOpp + ' ''' + @searchData + ''' ) as d'
+		'(SELECT ' + @colName
+		+ ' from dbo.' + @tableName
+		+ ' where ' + @colName + ' ' + @searchOpp + ' ' + @searchData + ' ) as d'
 	
 	fetch next from c1
 	into @tableName, @colName
@@ -89,5 +91,3 @@ order by tableName, columnName
 --comment this out if you want to use the results before they are dropped
 IF OBJECT_ID('tempdb..##searchResults') IS NOT NULL
 	DROP TABLE ##searchResults
-
-
